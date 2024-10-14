0cdac88809d90db5a8ba2c49d1560cfa4cfa83c0
Ayres <ayresdevweb@gmail.com>
2024-10-14 10:21:22 -0300

    Aula: O que são condicionais e booleanos?

--- index.php
+++ index.php
@@ -12,10 +12,37 @@
     $titulo = $saudacao . ", Portifolio do " . $nome;
     $subtitulo = "Seja bem vindo ao meu portifólio!";
     $ano = 2024;
+
+    $projeto = "Meu Portifólio";
+    $finalizado = true;
+    $dataDoProjeto = "2024-10-11";
+    $descricao = "Meu primeiro portifólio. Escrito em PHP e HTML.";
   ?>
 
   <h1><?= $titulo; ?></h1>
   <p><?= $subtitulo; ?></p>
   <p><?= $ano; ?></p>
+
+  <hr />
+
+  <div
+    <?php if ( !((2024 - $ano) > 2) ): ?>
+      style="background-color: burlywood"
+    <?php endif; ?>
+  >
+    <h2><?= $projeto ?></h2>
+    <p><?= $descricao; ?></p>
+    <div>
+      <div><?= $dataDoProjeto ?></div>
+      <div> 
+        Projeto:
+        <?php if (!$finalizado) : ?>
+          <span style="color: red">⛔️ não finalizado</span>
+        <?php else : ?>    
+          <span style="color: green">✅ finalizado</span>
+        <?php endif; ?>
+      </div>
+    </div>
+  </div>
 </body>
 </html>
