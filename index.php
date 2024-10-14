93f3ff3976e1d21fff21aabc7dce87fc42701047
Ayres <ayresdevweb@gmail.com>
2024-10-14 09:37:29 -0300

    Aula: O que são variáveis?

--- index.php
+++ index.php
@@ -6,6 +6,16 @@
   <title>Meu Portifólio</title>
 </head>
 <body>
-  <h1><?php echo "Portifólio do Ayres";  ?></h1>
+  <?php 
+    $nome = "Ayres";
+    $saudacao = "Oi";
+    $titulo = $saudacao . ", Portifolio do " . $nome;
+    $subtitulo = "Seja bem vindo ao meu portifólio!";
+    $ano = 2024;
+  ?>
+
+  <h1><?= $titulo; ?></h1>
+  <p><?= $subtitulo; ?></p>
+  <p><?= $ano; ?></p>
 </body>
 </html>
